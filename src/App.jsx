71d718271f8006114136d837b1ae8924f9c0ffd6
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-10-06 16:02:02 +0200

    feat: add Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import {RouterProvider, Route, createBrowserRouter, createRoutesFromElements} from "react-router-dom"
+import Home from "./pages/Home"
+import Quizz from "./pages/Quizz"
 
 
 function App() {
+
+  const router = createBrowserRouter(createRoutesFromElements(
+    <>
+      <Route path="/"  element={<Home />} />
+      <Route path="quizz" element={<Quizz />} />
+    </>
+  ))
  
   return (
-    <div className="bg-red-500">
-      <p>app here</p>
+    <div className="flex flex-col min-h-screen">
+      <RouterProvider router={router} />
     </div>
   )
 }
---
+++ src/pages/Quizz.jsx
@@ -0,0 +1,7 @@
+export default function Quizz(){
+    return(
+        <main>
+            <h1>Quizz here</h1>
+        </main>
+    )
+}
